d26318cd23ec0a39eae7dcc2bb4e5779427207f8
Luke Bouch <lukebouch2014@gmail.com>
2021-07-09 12:00:56 -0400

    fixed border

--- resources/views/components/house-card.blade.php
+++ resources/views/components/house-card.blade.php
@@ -3,8 +3,10 @@
         <h3 class="mb-0 mt-0">RiverStone Church</h3>
         <span>led by Nicole McCready</span>
         <p class="mt-3 mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis erat neque, a eleifend mauris mattis ac. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi at augue nibh. Vestibulum fringilla, ex in lobortis rhoncus, augue est facilisis mauris, tristique varius nibh nulla eget nunc. Mauris ut ligula massa. Vestibulum sit amet massa porttitor, ornare quam sit amet, ultrices eros. In erat justo, dictum eget maximus ac, placerat in nisl. Morbi aliquam tristique ultricies.</p>
-        <a class="p-4 mr-3 bg-blue-800 text-white" href="/">Contact Leader</a>
-        <a class="p-4 bg-gray-200 text-blue-800" href="/">Change Time</a>
+        <div class="text-center md:text-left">
+            <a class="p-4 mb-2 inline-block bg-blue-800 text-white" href="/">Contact Leader</a>
+            <a class="p-4 inline-block bg-gray-200 text-blue-800" href="/">Change Time</a>
+        </div>
     </div>
     <div class="md:h-screen md:overflow-y-scroll col-span-2 lg:col-span-1 text-center border">
         @php
@@ -20,8 +22,10 @@
                     $time = ($i - 12) . 'pm'
                 @endphp
             @endif
-            <div class="pt-2 border-t font-bold">{{ $time }}</div>
-            <div class="mb-2">{{ $faker->name() }}</div>
+            <div class="border-b">
+                <div class="pt-2 font-bold">{{ $time }}</div>
+                <div class="mb-2">{{ $faker->name() }}</div>
+            </div>
         @endfor
     </div>
 </div>
--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,9 +1,9 @@
 <nav class="bg-gray-50">
-    <div class="w-2/3 m-auto flex justify-between">
+    <div class="w-2/3 m-auto md:flex justify-between">
         <a href="/" class="flex-shrink-0">
             <img class="h-24 -ml-3" src="assets/logo.png" alt="">
         </a>
-        <ul class="flex items-center">
+        <ul class="pb-3 md:p-0 md:flex text-center items-center">
             <li><a class="m-3" href="/">Profile</a></li>
             <li><a class="m-3" href="/">Logout</a></li>
         </ul>
