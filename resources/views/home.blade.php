fa17da6f7524bdd35601cf71538f082495c2b555
Luke Bouch <lukebouch2014@gmail.com>
2021-07-09 13:08:18 -0400

    added a join button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,12 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <h1>Dashboard</h1>
-    <a class="" href=""></a>
+    <div class="flex justify-between">
+        <h1 class="inline-block">Dashboard</h1>
+        <div>
+            <a class="btn-primary" href="/house/join">Join a House</a>
+        </div>
+    </div>
+    
     <x-house-card />
 @endsection
