7fc2b765015c9183733a16eeb6f731181faaf5d3
Luke Bouch <lukebouch2014@gmail.com>
2021-07-09 08:40:13 -0400

    fixed Hash error

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Session;
-use Hash;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Hash;
 class FrontendController extends Controller
 {
     //
