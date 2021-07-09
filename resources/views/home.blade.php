df61675e293fbc5c7e2ddc052e01987b45900036
Luke Bouch <lukebouch2014@gmail.com>
2021-07-09 13:09:03 -0400

    added flex-wrap to the button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <div class="flex justify-between">
+    <div class="flex flex-wrap justify-between">
         <h1 class="inline-block">Dashboard</h1>
         <div>
             <a class="btn-primary" href="/house/join">Join a House</a>
