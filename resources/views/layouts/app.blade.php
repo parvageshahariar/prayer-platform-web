502445c22b731e4909205aae7e3bcb46b4996b45
Luke Bouch <lukebouch2014@gmail.com>
2021-07-09 10:43:14 -0400

    Improved main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'The Prayer Platform' }}</title>
+    <title>@yield('title') - The Prayer Platform</title>
 
     <link rel="stylesheet" href="css/app.css">
 </head>
 <body class="font-sans">
     <x-navigation></x-navigation>
-    <div class="container w-4/5 m-auto pt-5">
+    <div class="container w-2/3 m-auto pt-8">
         @yield('content')
     </div>
 </body>
