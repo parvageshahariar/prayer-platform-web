b2c32582ef9df05be657945a9169bf496af5856f
Luke Bouch <lukebouch2014@gmail.com>
2021-07-13 13:45:35 -0400

    fixed form action

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,7 +6,8 @@
     <h1 class="mb-0">Profile</h1>
     <span>Let's keep that info up to date!</span>
 
-    <form class="w-1/2 mt-5 grid grid-cols-2 gap-3" action="/">
+    <form class="w-1/2 mt-5 grid grid-cols-2 gap-3">
+        @csrf
         <input class="col-span-1" type="text" name="first_name" placeholder="First Name">
         <input class="col-span-1" type="text" name="last_name" placeholder="Last Name">
         <input class="col-span-2" type="email" name="email" placeholder="Email">
