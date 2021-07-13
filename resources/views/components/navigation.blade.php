1e0ba73b69fab4889ccfb588312b8f395a8c2bfb
Luke Bouch <lukebouch2014@gmail.com>
2021-07-13 15:23:37 -0400

    fixed navigation logo
    The navigation logo was not showing up on views that were inside of a folder. I had to use the built-in asset() function to reference the logo source.

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav class="w-3/4 m-auto pl-8 pr-8 mt-3 bg-gray-50 rounded-lg">
     <div class=" m-auto md:flex justify-between">
         <a href="/" class="flex-shrink-0">
-            <img class="max-h-24 md:-ml-3" src="assets/logo.png" alt="">
+            <img class="max-h-24 md:-ml-3" src="{{ asset('assets/logo.png') }}" alt="">
         </a>
         <ul class="pb-3 md:p-0 md:flex text-center items-center">
             <li><a class="m-3" href="{{ route('dashboard') }}">Dashboard</a></li>
