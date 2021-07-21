4e2b9a4b30fe9ac6f0ab65ca4c6078825b47601a
Luke Bouch <lukebouch2014@gmail.com>
2021-07-21 13:29:40 -0400

    added user data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Organization;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index() {
         $users = User::all();
         $organizations = Organization::all();
-        return view('dashboard')->with(compact('users', 'organizations'));
+        $user = Auth::user();
+        return view('dashboard')->with(compact('user','users', 'organizations'));
     }
 }
