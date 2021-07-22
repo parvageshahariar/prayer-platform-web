a4431d34794c46e7350eebc9ec6d7edd27db5c3b
Luke Bouch <lukebouch2014@gmail.com>
2021-07-22 13:40:34 -0400

    Rearranged organization page and added new group button

--- resources/views/organization/show.blade.php
+++ resources/views/organization/show.blade.php
@@ -1,6 +1,14 @@
 <x-app-layout :title="$organization->name">
     <h1>{{ $organization->name }}</h1>
     <p>{{ $organization->description }}</p>
+    <div class="flex flex-wrap">
+        <a class="btn-primary mr-2" href="{{ route('organizations.edit', ['organization'=>$organization]) }}">Edit</a>
+        <form action="{{ route('organizations.destroy', ['organization'=>$organization]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="btn-primary" type="submit">Delete</button>
+        </form>
+    </div>
     <h2>Groups</h2>
     @if ($groups)
         
@@ -12,12 +20,5 @@
     @else
         <p>This organization has no groups yet.</p>
     @endif
-    <div class="flex flex-wrap">
-        <a class="btn-primary mr-2" href="{{ route('organizations.edit', ['organization'=>$organization]) }}">Edit</a>
-        <form action="{{ route('organizations.destroy', ['organization'=>$organization]) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button class="btn-primary" type="submit">Delete</button>
-        </form>
-    </div>
+    <a class="btn-primary" href="{{ route('groups.create', ['organization'=>$organization->id]) }}">New</a>
 </x-app-layout>
