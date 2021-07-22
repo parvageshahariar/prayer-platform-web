c29c078c56c99c8aed8a93343b193af8021de7e6
Luke Bouch <lukebouch2014@gmail.com>
2021-07-22 13:02:19 -0400

    Fixed organization edit page

--- resources/views/organization/edit.blade.php
+++ resources/views/organization/edit.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout :title="Edit $organization->name">
+<x-app-layout title="Edit {{ $organization->name }}">
     <h1>Edit {{ $organization->name }}</h1>
     <form class="w-1/2 ml-auto mr-auto grid grid-cols-2 gap-3" action="{{ route('organizations.update', $organization->id) }}" method="POST">
         @method('PATCH')
