ebed74162cf37eaad7d7cd4266661f55271c5a92
Luke Bouch <lukebouch2014@gmail.com>
2021-07-22 10:33:41 -0400

    Fixed mobile UI

--- resources/views/organization/list.blade.php
+++ resources/views/organization/list.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout title="List of Organizations">
-    <div class="flex items-center justify-between">
+    <div class="flex flex-wrap items-center justify-between">
         <h1>Organizations</h1>
         <a class="btn btn-primary" href="{{ route('organizations.create') }}">Create an Organization</a>
     </div>
