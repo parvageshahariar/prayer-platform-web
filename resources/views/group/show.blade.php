7fb30db13de9e9e149260725f61cf009bed77b22
Luke Bouch <lukebouch2014@gmail.com>
2021-07-22 13:40:51 -0400

    Added edit and delete buttons on Group page

--- resources/views/group/show.blade.php
+++ resources/views/group/show.blade.php
@@ -1,4 +1,12 @@
 <x-app-layout :title="$group->name">
     <h1>{{ $group->name }}</h1>
     <p>{{ $group->description }}</p>
+    <div class="flex flex-wrap">
+        <a class="btn-primary mr-2" href="{{ route('groups.edit', ['group'=>$group]) }}">Edit</a>
+        <form action="{{ route('groups.destroy', ['group'=>$group]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="btn-primary" type="submit">Delete</button>
+        </form>
+    </div>
 </x-app-layout>
