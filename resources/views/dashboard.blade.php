0ea342d12cd520edff9f4ce6d668ec8886ed388c
Luke Bouch <lukebouch2014@gmail.com>
2021-07-22 10:35:30 -0400

    Added a list of organization a user is apart of on the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,15 @@
 <x-app-layout title="Dashboard">
     <h1 class="inline-block">Welcome back, {{ $user->first_name }}</h1>
+    @if (count($organizations) > 1)
+        <h2>Organizations</h2>
+        @foreach ($organizations as $organization)
+            <li>{{ $organization->name }}</li>
+        @endforeach
+    @else
+        <x-organization-card :data="$organizations[0]">
+            @foreach ($organizations[0]->groups as $group)
+                <li>{{ $group->name }}</li>
+            @endforeach
+        </x-organization-card>
+    @endif
 </x-app-layout>
